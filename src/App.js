bdefe33b9edfe20a1460088a4bc2ab66fb9abb65
davien21 <chidiebereekennia@gmail.com>
2021-08-09 03:02:16 +0100

    add routing to create and view employee pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import CreateEmployee from "./pages/createEmployee/index";
+import { Switch, Route, Redirect } from "react-router-dom";
+
 import Header from "./components/header/index";
+import CreateEmployee from "./pages/createEmployee/index";
+import ViewEmployees from "./pages/viewEmployees/index";
+import CurrentPage from "./components/CurrentPage/index";
 
 function App() {
   return (
-    <div className="container">
+    <>
       <Header />
-      <CreateEmployee />
-    </div>
+      <CurrentPage>
+        <Switch>
+          <Route path="/employees/create" component={CreateEmployee} />
+          <Route path="/employees" component={ViewEmployees} />
+
+          <Redirect from="/" exact to="/employees/create"></Redirect>
+          <Redirect to="/not-found" />
+        </Switch>
+      </CurrentPage>
+    </>
   );
 }
 
