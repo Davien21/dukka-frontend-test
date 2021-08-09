4ae2ff40c1b44a06115e046a240da66141c0c393
davien21 <chidiebereekennia@gmail.com>
2021-08-09 02:17:56 +0100

    First commit - add header to the app root

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CreateEmployee from "./pages/createEmployee/index";
+import Header from "./components/header/index";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <CreateEmployee />
+    </div>
+  );
+}
+
+export default App;
