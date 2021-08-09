f40d5752779d94ae75edb49efc6fcf9c8f5e609f
davien21 <chidiebereekennia@gmail.com>
2021-08-09 04:37:19 +0100

    add view employees page

---
+++ src/pages/viewEmployees/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EmployeeListing from "./../../components/employeeListing/index";
+
+function ViewEmployees(props) {
+  return (
+    <div>
+      <EmployeeListing />
+    </div>
+  );
+}
+
+export default ViewEmployees;
