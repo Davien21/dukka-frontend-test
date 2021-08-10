756ac19ef707c91a0273f905b5f9f1ef7ed622e8
davien21 <chidiebereekennia@gmail.com>
2021-08-10 20:00:20 +0100

    setup pagination and employees listing

--- src/pages/viewEmployees/index.jsx
+++ src/pages/viewEmployees/index.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import EmployeeListing from "./../../components/employeeListing/index";
+import React, { useState } from "react";
+import ProfileCardsTable from "./../../components/profileCardsTable/index";
+import Pagination from "./../../components/pagination/index";
+import { paginate } from "../../utils/paginate";
 
-function ViewEmployees(props) {
+function ViewEmployees({ employees }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize] = useState(3);
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+  console.log();
+  const movies = paginate(employees, currentPage, pageSize);
+
   return (
     <div>
-      <EmployeeListing />
+      <p className="h5 mb-4">EMPLOYEES</p>
+      <ProfileCardsTable employees={movies} />
+      <Pagination
+        itemsCount={employees.length}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
