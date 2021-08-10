23427bf280a4243d11b6b8c6bf8be9a1716d685f
davien21 <chidiebereekennia@gmail.com>
2021-08-10 14:52:00 +0100

    add toast container, refactor code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import { Switch, Route, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Header from "./components/header/index.jsx";
 import CurrentPage from "./components/currentPage/index.jsx";
-import CreateEmployee from "./pages/createEmployee/index.jsx";
-import ViewEmployees from "./pages/viewEmployees/index.jsx";
+import Employees from "./components/employees/index.jsx";
 
 function App() {
   return (
-    <div className="app-container">
-      <Header />
-      <CurrentPage>
-        <Switch>
-          <Route path="/employees/create" component={CreateEmployee} />
-          <Route path="/employees/listings" component={ViewEmployees} />
-
-          <Redirect from="/" exact to="/employees/create"></Redirect>
-          <Redirect to="/not-found" />
-        </Switch>
-      </CurrentPage>
-    </div>
+    <>
+      <ToastContainer />
+      <div className="app-container">
+        <Header />
+        <CurrentPage>
+          <Switch>
+            <Route path="/employees/" component={Employees} />
+            <Redirect from="/" exact to="/employees/listings"></Redirect>
+            <Redirect to="/not-found" />
+          </Switch>
+        </CurrentPage>
+      </div>
+    </>
   );
 }
 
