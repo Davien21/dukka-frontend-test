71216411324580d7f4cf046ce6564931f00618f2
davien21 <chidiebereekennia@gmail.com>
2021-08-10 10:59:20 +0100

    refactor code

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import TopNav from "./../navigation/topNav/index";
 import SideNav from "./../navigation/sideNav/index";
+import LetterLogo from "./../letterLogo/index";
 
 function Header(props) {
   return (
     <header className="container  ">
-      <div className="d-flex flex-wrap align-items-center">
+      <nav className="d-flex flex-wrap align-items-center py-2 justify-content-between">
         <TopNav />
         <SideNav />
-        <div className="col-auto text-right ml-3 my-2 letter-logo">
-          <span className="d-block">SAMASYS</span>
-          <span className="pr-3">combat salary fraud</span>
-        </div>
-      </div>
-      <hr className="row"/>
+        <LetterLogo />
+      </nav>
+      <hr className="row" />
     </header>
   );
 }
