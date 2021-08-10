67b3415a4e120526d676ef1e86b75f9b8c01305f
davien21 <chidiebereekennia@gmail.com>
2021-08-10 20:02:01 +0100

    pass employees state down to children

--- src/components/employees/index.jsx
+++ src/components/employees/index.jsx
@@ -1,23 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import CreateEmployee from "./../../pages/createEmployee/index";
 import ViewEmployees from "./../../pages/viewEmployees/index";
 import EmployeeProfile from "./../../pages/employeeProfile/index";
-import { getAllEmployees } from "../../services/employees";
+import { getEmployees } from "../../services/employees";
 
 function Employees(props) {
-  const [employees, setEmployees] = useState(getAllEmployees);
+  const [employees, setEmployees] = useState(getEmployees);
+  
 
-  console.log(employees);
   return (
     <div>
       <Switch>
-        <Route path="/employees/create" component={CreateEmployee} />
-        <Route path="/employees/listings" exact component={ViewEmployees} />
+        <Route
+          path="/employees/create"
+          exact
+          render={(props) => (
+            <CreateEmployee
+              {...props}
+              employees={employees}
+              onUpdateEmployees={setEmployees}
+            />
+          )}
+        />
+        <Route
+          path="/employees/listings"
+          exact
+          render={(props) => <ViewEmployees {...props} employees={employees} />}
+        />
         <Route
           path="/employees/listings/:id"
           exact
-          component={EmployeeProfile}
+          render={(props) => (
+            <EmployeeProfile {...props} employees={employees} />
+          )}
         />
       </Switch>
     </div>
