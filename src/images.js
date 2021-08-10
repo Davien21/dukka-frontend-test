e38f3b326116dd42157c0ac67e9a141f384c4a10
davien21 <chidiebereekennia@gmail.com>
2021-08-10 22:19:46 +0100

    add calender image

--- src/images.js
+++ src/images.js
@@ -1,7 +1,13 @@
 const menu = "/imgs/svgs/menu.svg";
 const missingPerson = "/imgs/svgs/confusion.svg";
+const editImage = "/imgs/svgs/edit.svg";
+const deleteImage = "/imgs/svgs/delete.svg";
+const calendarImage = "/imgs/svgs/calendar.svg";
 
 module.exports = {
   menu,
   missingPerson,
+  editImage,
+  deleteImage,
+  calendarImage,
 };
