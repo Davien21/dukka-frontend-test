e845f9bd250fa769b524fce193b618908170c458
davien21 <chidiebereekennia@gmail.com>
2021-08-10 08:32:29 +0100

    finish side nav setup

--- src/components/navigation/sideNav/index.jsx
+++ src/components/navigation/sideNav/index.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Overlay from "../../overlay";
+import MenuToggle from "./menuToggle";
+import SideBar from "./navigation";
+import { useLocation } from "react-router-dom";
 
 function SideNav(props) {
-  return <div className="d-md-none">Hi Side nav!</div>;
+  const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleSideNavToggle = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location]);
+
+  return (
+    <div className="d-md-none">
+      <MenuToggle onToggleMenu={handleSideNavToggle} />
+      <Overlay className="" isOpen={isOpen} onClick={() => setIsOpen(false)} />
+      <SideBar onCloseSideBar={() => setIsOpen(false)} isOpen={isOpen} />
+    </div>
+  );
 }
 
 export default SideNav;
