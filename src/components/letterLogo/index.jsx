680f0042c7458791346453659379f1d65a8bdfbb
davien21 <chidiebereekennia@gmail.com>
2021-08-10 10:59:38 +0100

    abstract the letter logo into a component

---
+++ src/components/letterLogo/index.jsx
@@ -0,0 +1,13 @@
+function LetterLogo() {
+  return (
+    <div className="d-flex text-right">
+      <div className="left-border"></div>
+      <div className="letter-logo">
+        <span className="d-block">SAMASYS</span>
+        <span className="pr-3">combat salary fraud</span>
+      </div>
+    </div>
+  );
+}
+
+export default LetterLogo;
