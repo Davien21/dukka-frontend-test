7088b0ce82e6ab28368a327a07b48eb9a6613917
davien21 <chidiebereekennia@gmail.com>
2021-08-10 22:23:45 +0100

    refactor code

--- src/pages/employeeProfile/index.jsx
+++ src/pages/employeeProfile/index.jsx
@@ -1,21 +1,16 @@
 import ProfileCard from "./../../components/profileCard/index";
-import { missingPerson } from "../../images";
+import InvalidEmployee from "./../../components/invalidEmployee/index";
+import LoanCardsTable from "./../../components/loanCardsTable/index";
 
 function EmployeeProfile({ employees, match }) {
   const id = match.params.id;
   const employee = employees.find((employee) => employee.id === id);
-  if (!employee)
-    return (
-      <div className="d-flex flex-column mx-auto justify-content-center align-items-center">
-        <img height={200} src={missingPerson} alt="" />
-        <span className="h5 mt-4">Invalid Employee</span>
-      </div>
-    );
+  if (!employee) return <InvalidEmployee />;
+
   return (
     <div className="container-fluid">
-      <p className="row h5 mb-4">EMPLOYEE PROFILE</p>
       <ProfileCard employee={employee} />
-      <p className="row h5 my-4">LOAN RECORD</p>
+      <LoanCardsTable />
     </div>
   );
 }
